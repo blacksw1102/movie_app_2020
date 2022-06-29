a6fa5a88208bda45ecad7702de56fab5efb10da7
=blacksw <blacksw1102@gmail.com>
2022-06-29 13:45:19 +0900

    8-5 저장

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './About.css';
 
-function About() {
+function About(props) {
+  console.log(props);
   return (
     <div className="about__container">
       <span>
-        "Freedom is the freedom to say that two plus two make four. If this is granted, all else follows."
+        "Freedom is the freedom to say that two plus two make four.
+        If this is granted, all else follows."
       </span>
       <span>- George Orwell, 1984</span>
     </div>
---
+++ src/routes/Detail.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+
+    if (location.state === undefined) {
+      history.push("/");
+    }
+
+    console.log(this.props);
+  }
+
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Detail;
